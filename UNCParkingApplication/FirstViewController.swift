0e7e308ee716050dec970f640b3df1245efce8f6
Thomas Shealy <thomasshealy32@gmail.com>
2019-02-04 17:42:29 -0500

    Initial Commit

---
+++ UNCParkingApplication/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  UNCParkingApplication
+//
+//  Created by Thomas Shealy on 2/4/19.
+//  Copyright © 2019 UNC Transportation and Parking. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UNCParkingApplication/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  UNCParkingApplication
+//
+//  Created by Thomas Shealy on 2/4/19.
+//  Copyright © 2019 UNC Transportation and Parking. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
