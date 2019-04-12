764b995ab9992375ab68a10d99be219fd93f79e3
David Gallub <dgallub1@gmail.com>
2019-04-12 01:11:29 -0400

    added correct more info functionality

--- UNCParkingApplication/MoreInfoController.swift
+++ UNCParkingApplication/MoreInfoController.swift
@@ -14,38 +14,40 @@ class MoreInfoController: UIViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func moreInfoButtonPressed(_ sender: Any) {
-        
+    @IBAction func buttonOnePressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://move.unc.edu/about/online-services/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func buttonTwoPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://move.unc.edu/transit/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func buttonThreePressed(_ sender: Any) {
         UIApplication.shared.open(URL(string: "https://move.unc.edu/parking/")! as URL, options: [:], completionHandler: nil)
-        
     }
     
-    @IBAction func employeeButtonPressed(_ sender: Any) {
-        
-        UIApplication.shared.open(URL(string: "https://move.unc.edu/parking/employee-parking/")! as URL, options: [:], completionHandler: nil)
-        
+    @IBAction func buttonFourPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://move.unc.edu/cap/")! as URL, options: [:], completionHandler: nil)
     }
     
-    @IBAction func studentButtonPressed(_ sender: Any) {
-        
-        UIApplication.shared.open(URL(string: "https://move.unc.edu/parking/student-parking/")! as URL, options: [:], completionHandler: nil)
-        
+    @IBAction func buttonFivePressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://move.unc.edu/about/pricing/")! as URL, options: [:], completionHandler: nil)
     }
     
-    @IBAction func visitorButtonPressed(_ sender: Any) {
-        
-        UIApplication.shared.open(URL(string: "https://move.unc.edu/parking/visitor-parking/")! as URL, options: [:], completionHandler: nil)
-        
+    @IBAction func buttonSixPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://move.unc.edu/events/")! as URL, options: [:], completionHandler: nil)
     }
     
-    @IBAction func meterButtonPressed(_ sender: Any) {
-        
-        UIApplication.shared.open(URL(string: "https://move.unc.edu/parking/meters/")! as URL, options: [:], completionHandler: nil)
-        
+    @IBAction func buttonSevenPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://itunes.apple.com/app/apple-store/id751972942?mt=8")! as URL, options: [:], completionHandler: nil)
     }
     
+    @IBAction func buttonEightPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://parkmobile.app.link/SYLSYDYShR")! as URL, options: [:], completionHandler: nil)
+    }
     
-    
-    
+    @IBAction func buttonNinePressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://move.unc.edu/")! as URL, options: [:], completionHandler: nil)
+    }
     
 }
